d0795a569507bd39f843d4bcdf2e0febca689468
Daniel <daniel.moderiano@outlook.com>
2021-09-26 20:32:00 +0930

    Style navbar (basic changes only)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className="nav">
+      <ul className="nav__list">
+        <li className="nav__list-item">
+          <Link className="nav__link" to="/">Home</Link>
+        </li>
+        <li className="nav__list-item">
+          <Link className="nav__link" to="/shop">Shop</Link>
+        </li>
+        <li className="nav__list-item">
+          <Link className="nav__link" to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
