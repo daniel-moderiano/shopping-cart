6010453986ef05fcd3f330c9fec3bbcbab25aa23
Daniel <daniel.moderiano@outlook.com>
2021-09-26 15:31:58 +0930

    Add imports to Routes.js

---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        {/* Routes placed here */}
+      </Switch>
+    </Router>
+  )
+}
+
+export default Routes
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './Routes';
 
+// Render the Routes component instead of App. Note that the Routes component does not actually add a DOM element, but instead outlines the control of component rendering
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
