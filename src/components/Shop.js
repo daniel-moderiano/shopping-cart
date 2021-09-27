2722a962a546524ba874aa91ca92b3fa286918e8
Daniel <daniel.moderiano@outlook.com>
2021-09-27 15:27:30 +0930

    Add products array in Shop.js state

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ShopCard from './ShopCard';
 
 const Shop = () => {
+
+  const [products, setProducts] = useState([
+    {
+      product: 'Magic Carpfin',
+      price: 40.00,
+      img: '',
+    },
+    {
+      product: 'Recess Diplomat',
+      price: 48.00,
+      img: '',
+    },
+    {
+      product: 'YoYoFactory SuperStar',
+      price: 70.00,
+      img: '',
+    },
+    {
+      product: 'YoYoFactory Replay',
+      price: 15.00,
+      img: '',
+    },
+    {
+      product: 'YoYo King Ghost',
+      price: 25.00,
+      img: '',
+    },
+    {
+      product: 'YoYo King Watcher',
+      price: 20.00,
+      img: '',
+    },
+    {
+      product: 'YoYoFactory Boss',
+      price: 25.00,
+      img: '',
+    },
+    {
+      product: 'YoYoFactory Grind Machine',
+      price: 40.00,
+      img: '',
+    },
+    {
+      product: 'Duncan Barracuda',
+      price: 40.00,
+      img: '',
+    },
+    {
+      product: 'Magic April',
+      price: 40.00,
+      img: '',
+    },
+    {
+      product: 'One Drop Burnside',
+      price: 95.00,
+      img: '',
+    },
+    {
+      product: 'Vosun Galsang',
+      price: 65.00,
+      img: '',
+    },
+
+  ]);
+
   return (
     <div>
       <h1>Shop</h1>
+      <h2>Advanced Yo-Yos</h2>
+      <ShopCard />
+      <ShopCard />
+      <ShopCard />
+      <ShopCard />
     </div>
   );
 }
--- src/components/ShopCard.js
+++ src/components/ShopCard.js
@@ -4,8 +4,8 @@ const ShopCard = () => {
   return (
     <div className="product">
       <img className="product__img" src="" alt="" />
-      <h2 className="product__name">Product name</h2>
-      <p className="product__price">$49.99 AUD</p>
+      <h3 className="product__name">Product name</h3>
+      <div className="product__price">$49.99 AUD</div>
       <div className="product__overlay">
         <button className="product__add-btn">Add to Cart</button>
       </div>
