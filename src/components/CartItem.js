2f4bae088998c7457eedfb1d063aed7d9c07df0a
Daniel <daniel.moderiano@outlook.com>
2021-09-27 20:12:42 +0930

    Update cartItem.js to include input field and increment/decrement btns (not yet functioning)

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,7 +6,11 @@ const CartItem = ({ item }) => {
       <img className="cartItem__img" src="" alt="" />
       <h3 className="cartItem__name">{item.name}</h3>
       <div className="cartItem__price">${item.price} AUD</div>
-      <div className="cartItem__quantity">{item.quantity}</div>
+      {/* TODO: switch the quantity to an input field, and use state to have it update quantity values on user typing or button click */}
+      <label htmlFor="quantity">Quantity</label>
+      <input type="text" name="quantity" id="quantity" value={item.quantity}/>
+      <button className="cartItem__increment">+</button>
+      <button className="cartItem__deccrement">-</button>
       <button className="cartItem__remove">Remove</button>
     </div>
   );
