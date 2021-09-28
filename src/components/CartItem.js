526d8d20621fd3d216925311cdc7050ba9902b3c
Daniel <daniel.moderiano@outlook.com>
2021-09-28 20:57:49 +0930

    Add input validation/modifiers for cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
 const CartItem = ({ item, handleChange }) => {
+
+  const avoidBlankQuantityInput = (event) => {
+    if (event.target.value === "") {
+      event.target.value = 0;
+    }
+  };
+
+  const removeLeadingZeroes = (event) => {
+    
+    if ((event.target.value).length > 1 && event.target.value[0] === "0") {
+      console.log('running');
+      event.target.value = (event.target.value).slice(1);
+    }
+  }
+
   return (
     <div className="cartItem">
       <img className="cartItem__img" src="" alt="" />
       <h3 className="cartItem__name">{item.name}</h3>
       <div className="cartItem__price">${item.price} AUD</div>
-      {/* TODO: switch the quantity to an input field, and use state to have it update quantity values on user typing or button click */}
       <label htmlFor="quantity">Quantity</label>
-      <input type="number" name="quantity" id="quantity" value={item.quantity} onChange={(event) => handleChange(event, item)}/>
+      <input 
+        type="number" 
+        name="quantity" 
+        id="quantity" 
+        value={item.quantity} 
+        onChange={(event) => {
+          avoidBlankQuantityInput(event);
+          removeLeadingZeroes(event);
+          handleChange(event, item);
+        }}
+      />
       <button className="cartItem__increment">+</button>
       <button className="cartItem__deccrement">-</button>
       <button className="cartItem__remove">Remove</button>
