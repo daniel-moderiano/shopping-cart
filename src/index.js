c9db2ed7ecfd723942477872457604ccbfe96c6d
Daniel <daniel.moderiano@outlook.com>
2021-10-01 23:03:48 +0930

    Swithc to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App /> 
-    </BrowserRouter>
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
