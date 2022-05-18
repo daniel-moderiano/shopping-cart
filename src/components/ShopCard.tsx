dcec20c80638e6bcb1ca680a4248c321f4bf0eb5
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-05-18 11:33:27 +0930

    Convert remainingh components to valid typescript

--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import { Product } from '../App'
 
-const ShopCard = ({ name, price, id, handleClick, product, src }) => {
+interface ShopCardProps {
+  handleClick: (product: Product) => void;
+  product: Product;
+}
 
-  const changeAddToCartBtn = (event) => {
-    event.target.textContent = 'Added!';
+const ShopCard = ({ handleClick, product }: ShopCardProps) => {
+
+  const changeAddToCartBtn = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const btn = event.target as HTMLButtonElement;
+    btn.textContent = 'Added!';
     setTimeout(() => {
-      event.target.textContent = 'Add to Cart';
+      btn.textContent = 'Add to Cart';
     }, 500)
   };
 
   return (
     <div className="product">
       <div className="product__img-container">
-        <img className="product__img" src={src} alt="Yo-Yo" />
+        <img className="product__img" src={product.img} alt="Yo-Yo" />
       </div>
-      <h3 className="product__name">{name}</h3>
-      <div className="product__price">$ {price}</div>
+      <h3 className="product__name">{product.name}</h3>
+      <div className="product__price">$ {product.price}</div>
       {/* Add a page alert on click to indicate the button has been pressed */}
       <button onClick={(event) => { handleClick(product); changeAddToCartBtn(event) }} className="product__add-btn">Add to Cart</button>
     </div>
